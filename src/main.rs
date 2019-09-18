cd6feedc32037ee47944300225a8490ac05d328a
Chris MacNaughton <chris@centaurisolutions.nl>
2019-09-18 16:26:52 +0200

    Handle Result<T>

--- src/main.rs
+++ src/main.rs
@@ -169,7 +169,7 @@ fn debdiff(name: &str, release: &str, patch: &str, upstream: Option<&str>) -> Re
 fn build(name: &str) -> Result<()> {
     println!("Building {}...", name);
 
-    Package::new(name, get_current_dir()).build()
+    Package::new(name, get_current_dir())?.build()
 }
 
 /// Clones package.
